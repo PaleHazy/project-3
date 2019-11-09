4142b6ee16c1993708e609fcd2d81a97de0a2f11
PaleHazy <austinrpg@live.com>
2019-11-09 13:45:32 -0600

    create react app and homepage files

---
+++ src/homepage.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './homepage.styles.scss'
+export const Homepage = () => (
+
+    <div className='homepage'>
+        <div className= 'menu-item'>
+            <div className="content">
+                <h1 className='title'>Hats</h1>
+                <span className='subtitle'>SHOPP NOW</span>
+            </div>
+        </div>
+        <div className= 'menu-item'>
+            <div className="content">
+                <h1 className='title'>Jackets</h1>
+                <span className='subtitle'>SHOPP NOW</span>
+            </div>
+        </div>
+        <div className= 'menu-item'>
+            <div className="content">
+                <h1 className='title'>WOMENS</h1>
+                <span className='subtitle'>SHOPP NOW</span>
+            </div>
+        </div>
+        <div className= 'menu-item'>
+            <div className="content">
+                <h1 className='title'>MENS</h1>
+                <span className='subtitle'>SHOPP NOW</span>
+            </div>
+        </div>
+    </div>
+)
