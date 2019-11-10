ce2a5be38a63e369687f18b146c1d5c760d79fcb
PaleHazy <austinrpg@live.com>
2019-11-09 18:11:22 -0600

    fixed the html/jsx on the homepage to fit with the sass code

--- src/homepage.component.jsx
+++ src/homepage.component.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
-import './homepage.styles.scss'
-export const Homepage = () => (
+import React from "react";
+import "./homepage.styles.scss";
 
-    <div className='homepage'>
-        <div className= 'menu-item'>
+export const Homepage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
             <div className="content">
-                <h1 className='title'>Hats</h1>
-                <span className='subtitle'>SHOPP NOW</span>
+                <h1 className="title">HATS</h1>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-        <div className= 'menu-item'>
+        <div className="menu-item">
             <div className="content">
-                <h1 className='title'>Jackets</h1>
-                <span className='subtitle'>SHOPP NOW</span>
+                <h1 className="title">CLOTHES</h1>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-        <div className= 'menu-item'>
+        <div className="menu-item">
             <div className="content">
-                <h1 className='title'>WOMENS</h1>
-                <span className='subtitle'>SHOPP NOW</span>
+                <h1 className="title">DESIGNER</h1>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
-        <div className= 'menu-item'>
+        <div className="menu-item">
             <div className="content">
-                <h1 className='title'>MENS</h1>
-                <span className='subtitle'>SHOPP NOW</span>
+                <h1 className="title">OIL</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">WOMEN</h1>
+                <span className="subtitle">SHOP NOW</span>
             </div>
         </div>
     </div>
-)
+  </div>
+);
