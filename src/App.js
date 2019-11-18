1cc45ba747b355d61b825608fb2562a821bd5766
PaleHazy <austinrpg@live.com>
2019-11-17 23:10:39 -0600

    user sign in and sign out features as such as redirecting when signed in

--- src/App.js
+++ src/App.js
@@ -1,43 +1,37 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import "./App.css";
 
-import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { setCurrentUser } from "./redux/user/user.actions";
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
+    console.log("props", this.props);
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          console.log("firebase snapshot", snapShot);
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-
-          console.log(this.state);
         });
-      }
 
-      this.setState({ currentUser: userAuth });
+      }
+      console.log("userAuth", userAuth);
+      setCurrentUser(userAuth);
     });
   }
 
@@ -48,15 +42,26 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => this.props.currentUser? (<Redirect to='/' />) : (<SignInAndSignUpPage/>)} />
         </Switch>
       </div>
     );
   }
 }
 
-export default App;
+const giveStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+const mapDispatchToProps = dispatch => {
+  console.log('dispatch',dispatch)
+  return {
+    setCurrentUser: user => {
+      console.log('user', user) 
+      return (dispatch(setCurrentUser(user)))}
+  };
+};
+export default connect(giveStateToProps, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {connect} from 'react-redux'
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -32,4 +32,9 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const giveStateToProps = state => {
+  console.log('state', state)
+  return ({
+   currentUser: state.user.currentUser
+})}
+export default connect(giveStateToProps)(Header);
