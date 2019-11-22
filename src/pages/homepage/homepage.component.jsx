51d7eca5d07dddd7cf6566e787f9873d0d050ae6
PaleHazy <austinrpg@live.com>
2019-11-21 20:01:57 -0600

    installed dependencies and fixed them

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -6,11 +6,7 @@ import './homepage.styles.scss';
 
 const HomePage = () => (
   <div className='homepage'>
-<<<<<<< HEAD
-  
-=======
   <ChatbotPedia />
->>>>>>> 26653eca6fcbb0c059f06205be8379992013c176
     <Directory />
   </div>
 );
