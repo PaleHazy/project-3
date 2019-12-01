be1a75e6a5e02fa9fbb933adb6dafa8bb3aa663b
PaleHazy <austinrpg@live.com>
2019-12-01 00:40:36 -0600

    fixed stripe connetivity for deployment

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
@@ -10,8 +9,8 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(cors());
 
